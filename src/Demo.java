22d37f5b8a980b7f34b3cbef0ec627517bba0e5c
Gyanesh_Saikhedkar <gyanesh.saikhedkar@vuliv.com>
2019-09-16 15:37:52 +0530

    add functionn add

--- src/Demo.java
+++ src/Demo.java
@@ -4,8 +4,14 @@ public class Demo {
 	{
 		System.out.println("code is running");
 	}
+	public void add()
+	{
+		int a=5, b = 6;
+		System.out.println(""+(a+b));
+	}
 public static void main(String[] args) {
 	Demo d = new Demo();
 	d.show();
+	d.add();
 }
 }
